fd57afaae59a1c56126c59557cc5e1ab0fe95b16
Alisyem <152813093+nintenfando@users.noreply.github.com>
2025-05-13 08:21:17 -0500

    Add files via upload

--- main.java
+++ main.java
@@ -1,19 +1,85 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.ListMessagesResponse;
 import com.google.api.services.gmail.model.Message;
-
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException, GeneralSecurityException {
-        // Gmail API setup code here
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "credentials.json";
+
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+            JSON_FACTORY,
+            new InputStreamReader(new FileInputStream(CREDENTIALS_FILE_PATH))
+        );
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+            .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+            .setAccessType("offline")
+            .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    public static void main(String[] args) {
+        try {
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName("Noti")
+                .build();
+
+            // Modified section: Get latest message TO YOU
+            ListMessagesResponse response = service.users().messages()
+                .list("me")
+                .setQ("is:inbox")  // Only messages in inbox
+                .setMaxResults(1)  // Get most recent
+                .execute();
+
+            if (response.getMessages() != null && !response.getMessages().isEmpty()) {
+                String messageId = response.getMessages().get(0).getId();
+                Message message = service.users().messages().get("me", messageId).execute();
+
+                String subject = message.getPayload().getHeaders().stream()
+                    .filter(h -> h.getName().equalsIgnoreCase("Subject"))
+                    .findFirst()
+                    .map(h -> h.getValue())
+                    .orElse("(No Subject)");
+
+                String sender = message.getPayload().getHeaders().stream()
+                    .filter(h -> h.getName().equalsIgnoreCase("From"))
+                    .findFirst()
+                    .map(h -> h.getValue())
+                    .orElse("Unknown Sender");
+
+                System.out.println("Latest message to you:");
+                System.out.println("Subject: " + subject);
+                System.out.println("From: " + sender);
+            } else {
+                System.out.println("No messages found in your inbox.");
+            }
+        } catch (IOException | GeneralSecurityException e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
